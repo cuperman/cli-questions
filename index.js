7cedb94b0ff0ef46db8af5e2b791cc5b9db8852d
Jeff Cooper <jeff@cuperman.net>
2017-07-08 09:00:56 -0700

    Ask questions

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 
 // jshint esversion: 6
 
+'use strict';
+
 const pkg = require('./package');
 const program = require('commander');
+const inquirer = require('inquirer');
 
 program
   .version(pkg.version)
@@ -13,9 +16,43 @@ program
   .option('-f, --force', 'Force push')
   .parse(process.argv);
 
-console.log(`sure:        ${program.sure}`);
-console.log(`base branch: ${program.base}`);
-console.log(`description: ${program.desc}`);
-console.log(`force push:  ${program.force}`);
+const questions = [{
+  type: 'confirm',
+  name: 'sure',
+  message: 'Are you sure?',
+  when: () => !program.sure
+}, {
+  type: 'list',
+  name: 'base',
+  message: 'Choose base branch',
+  choices: [
+    'track/ui',
+    'track/sp',
+    'track/connect'
+  ],
+  when: () => !program.base
+}, {
+  type: 'input',
+  name: 'desc',
+  message: 'Enter a description',
+  when: () => !program.desc
+}, {
+  type: 'confirm',
+  name: 'force',
+  message: 'Force push?',
+  when: () => !program.force
+}];
 
-process.exit(0);
+inquirer.prompt(questions).then(function (answers) {
+  let sure  = program.sure || answers.sure;
+  let base  = program.base || answers.base;
+  let desc  = program.desc || answers.desc;
+  let force = program.force || answers.force;
+
+  console.log(`sure:        ${sure}`);
+  console.log(`base branch: ${base}`);
+  console.log(`description: ${desc}`);
+  console.log(`force push:  ${force}`);
+
+  process.exit(0);
+});
